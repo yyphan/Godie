46b8ff41a4627a0cc94ab40c99dfa86c71ae0cd3
yyphan <jeromeyao1999@gmail.com>
2023-10-21 21:15:16 +0800

    Handle running

--- player.gd
+++ player.gd
@@ -8,6 +8,25 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+@export_group("Running")
+@export var _max_speed: int = 100
+## Set to true to ignore accelerations and always move at Max Speed
+@export var _is_instant_movement: bool = false;
+
+@export var _acceleration: int = 70
+@export var _midair_acceleration: int = 70
+
+## The rate the velocity decelerates to 0 when no input present (on ground)
+@export var _deceleration: int = 70 
+## The rate the velocity decelerates to 0 when no input present (mid air)
+@export var _midair_deceleration: int = 70
+
+## The rate the velocity decelerates and/or flips when input is opposite to current velocity (on ground)
+@export var _turn_speed: int = 70; 
+## The rate the velocity decelerates and/or flips when input is opposite to current velocity (mid air)
+@export var _midair_turn_speed: int = 70; 
+
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -16,13 +35,30 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	velocity.x = handle_running(delta, velocity.x, is_on_floor())
+	
 	move_and_slide()
+
+
+# Get horizontal input direction and return velocity after acceleration/turn/deceleration.
+func handle_running(delta_time: float, current_velocity_x: float, is_on_ground: bool) -> float:
+	var direction: float = Input.get_axis("move_left", "move_right")  # ranges between -1 and 1
+	if _is_instant_movement:
+		return direction * _max_speed
+		
+	var acceleration: int = _acceleration if is_on_ground else _midair_acceleration
+	var deceleration: int = _deceleration if is_on_ground else _midair_deceleration
+	var turn_speed: int = _turn_speed if is_on_ground else _midair_turn_speed
+	
+	var target_velocity_x: int = sign(direction) * _max_speed
+	var delta_speed: float
+	if direction != 0:
+		if sign(direction) == sign(current_velocity_x):  # accelerating
+			delta_speed = acceleration * delta_time
+		else:  # turning
+			delta_speed = turn_speed * delta_time
+	else:  # deceleration
+		delta_speed = deceleration * delta_time
+		
+	return move_toward(current_velocity_x, target_velocity_x, delta_speed)
