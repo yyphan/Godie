5fca9475d544d06e28fd5810de0dd5525f39ee97
yyphan <jeromeyao1999@gmail.com>
2023-10-27 16:19:38 +0800

    coyote time

--- player.gd
+++ player.gd
@@ -26,15 +26,17 @@ extends CharacterBody2D
 @export var _upward_gravity_multiplier: float = 1
 ## Scales the gravity when falling down (in the jump), e.g. set to 2 would be two times the normal gravity
 @export var _downward_gravity_multiplier: float = 1
+## Coyote time allows character to perform the jump within some time after falling off platform
+@export var _coyote_time_seconds: float = 0.2
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var _base_gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-var _cur_gravity: float = _base_gravity
+var _coyote_time_counter: float = 0
 
 func _physics_process(delta):
-	
 	velocity.x = handle_running(delta, velocity.x, is_on_floor())
 	velocity.y = handle_jumping(delta, velocity.y, is_on_floor())
+	velocity.y = apply_gravity(delta, velocity.y)
 	
 	move_and_slide()
 
@@ -64,19 +66,34 @@ func handle_running(delta_time: float, current_velocity_x: float, is_on_ground: 
 
 # Get jump input and update vertical velocity 
 func handle_jumping(delta_time: float, current_velocity_y: float, is_on_ground: bool) -> float:
-	calculate_gravity(current_velocity_y)
-	if Input.is_action_just_pressed("jump"):
-		return current_velocity_y - sqrt(2 * _jump_height * _cur_gravity)
-	return apply_gravity(delta_time, current_velocity_y)
+	if is_on_ground:
+		_coyote_time_counter = 0
+	elif _coyote_time_counter < _coyote_time_seconds + delta_time:
+		_coyote_time_counter += delta_time
+		
+	if Input.is_action_just_pressed("jump") && can_jump(is_on_ground):
+		var jump_force: float = sqrt(2 * _jump_height * calculate_gravity(current_velocity_y))
+		if current_velocity_y > 0:  # making jump during coyote time feels the same as jumping of platform
+			jump_force += current_velocity_y
+		current_velocity_y -= jump_force
 	
+	return current_velocity_y
+
 
 func apply_gravity(delta_time: float, current_velocity_y: float) -> float:
-	return current_velocity_y + _cur_gravity * delta_time
+	var cur_gravity: float = calculate_gravity(current_velocity_y)
+	return current_velocity_y + cur_gravity * delta_time
 		
 
 # Adjust gravity when jumping up and falling down
 func calculate_gravity(current_velocity_y: float):
 	if current_velocity_y > 0.01:  # falling down
-		_cur_gravity = _base_gravity * _downward_gravity_multiplier
+		return _base_gravity * _downward_gravity_multiplier
 	else:  # jumping up
-		_cur_gravity = _base_gravity * _upward_gravity_multiplier
+		return _base_gravity * _upward_gravity_multiplier
+
+
+# Can jump either on platform, or just falling off platform 
+func can_jump(is_on_ground: bool) -> bool:
+	return is_on_ground or _coyote_time_counter < _coyote_time_seconds
+	
