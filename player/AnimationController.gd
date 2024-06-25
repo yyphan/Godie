0764d9ca1b51827144922015696659febd8df874
yyphan <jeromeyao1999@gmail.com>
2024-06-25 22:48:05 +0800

    state machine POC
    动画里可以叫函数，参考player attack播放完之后我让他set_state(IDLE)

--- player/AnimationController.gd
+++ player/AnimationController.gd
@@ -2,21 +2,20 @@ extends Node
 
 @onready var player: Player = get_owner()
 
+enum PLAYER_ANIM_STATE {IDLE, ATTACK, SHIELD, HURT, RUN}
+var _cur_state: PLAYER_ANIM_STATE = PLAYER_ANIM_STATE.IDLE
+
+func set_state(new_state: PLAYER_ANIM_STATE):
+	_cur_state = new_state
+
 func is_running() -> bool:
-	return player.velocity.x != 0
+	return _cur_state == PLAYER_ANIM_STATE.RUN
 	
 func is_hurting() -> bool:
-	return player.is_hurt_state
-
-func is_attack_just_pressed() -> bool:
-	return Input.is_action_just_pressed("attack")
-
-
-func is_shield_just_pressed() -> bool:
-	return Input.is_action_just_pressed("shield")
+	return _cur_state == PLAYER_ANIM_STATE.HURT
 	
-	
-func is_shield_just_released() -> bool:
-	return Input.is_action_just_released("shield")
+func is_holding_shield() -> bool:
+	return _cur_state == PLAYER_ANIM_STATE.SHIELD
 
-
+func is_attacking() -> bool:
+	return _cur_state == PLAYER_ANIM_STATE.ATTACK
--- player/player.gd
+++ player/player.gd
@@ -2,17 +2,70 @@ extends CharacterBody2D
 
 class_name Player
 @export var knockback_speed := 150
-var is_hurt_state : bool = false
 var _base_gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+enum PLAYER_STATE {IDLE, ATTACK, SHIELD, HURT, RUN}
+var _cur_state: PLAYER_STATE = PLAYER_STATE.IDLE
+
 func _physics_process(delta):
-	if(!is_hurt_state):
+	_handle_input()
+	
+	if (_can_move()):
 		velocity.x = $MovementController.handle_running(delta, velocity.x, is_on_floor())
+		if (velocity.x != 0):
+			set_state(PLAYER_STATE.RUN)
+		else:
+			set_state(PLAYER_STATE.IDLE)
+		
+	if (_can_jump()):
 		velocity.y = $JumpController.handle_jumping(delta, velocity.y, is_on_floor())
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, knockback_speed * delta)
-		is_hurt_state = false
+		
 	move_and_slide()
+
+	
+func set_state(new_state: PLAYER_STATE):
+	if (_cur_state == new_state):
+		return
+	
+	match(new_state):
+		PLAYER_STATE.IDLE:
+			_cur_state = new_state
+			$AnimationController.set_state($AnimationController.PLAYER_ANIM_STATE.IDLE)
+		PLAYER_STATE.RUN:
+			_cur_state = new_state
+			$AnimationController.set_state($AnimationController.PLAYER_ANIM_STATE.RUN)
+		PLAYER_STATE.ATTACK:
+			_cur_state = new_state
+			velocity = Vector2.ZERO
+			$AnimationController.set_state($AnimationController.PLAYER_ANIM_STATE.ATTACK)
+		PLAYER_STATE.SHIELD:
+			_cur_state = new_state
+			velocity = Vector2.ZERO
+			$AnimationController.set_state($AnimationController.PLAYER_ANIM_STATE.SHIELD)
+		PLAYER_STATE.HURT:
+			_cur_state = new_state
+			velocity = Vector2.ZERO
+			$AnimationController.set_state($AnimationController.PLAYER_ANIM_STATE.HURT)
+
+
+func _handle_input():
+	if (_cur_state == PLAYER_STATE.HURT):
+		return;
+	
+	if (Input.is_action_just_pressed("attack")):
+		set_state(PLAYER_STATE.ATTACK)
+	elif (Input.is_action_just_pressed("shield")):
+		set_state(PLAYER_STATE.SHIELD)	
+	elif (Input.is_action_just_released("shield")):
+		set_state(PLAYER_STATE.IDLE)	
+
+
+func _can_move() -> bool:
+	return _cur_state != PLAYER_STATE.HURT && _cur_state != PLAYER_STATE.SHIELD && _cur_state != PLAYER_STATE.ATTACK
+
+
+func _can_jump() -> bool:
+	return _cur_state != PLAYER_STATE.HURT && _cur_state != PLAYER_STATE.ATTACK
 
 
 func _on_hurt_area_area_entered(area):
@@ -21,4 +74,4 @@ func _on_hurt_area_area_entered(area):
 	else:
 		velocity -= Vector2(-1,0.5) * knockback_speed
 	print("受伤啦")
-	is_hurt_state = true
+	set_state(PLAYER_STATE.HURT)
